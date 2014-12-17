0e65cd01a6b5307e77d86e0490c4267af6e4caeb
Erik Carlsson <info@code301.com>
2014-12-17 17:50:48 +0100

    "Init."

---
+++ lib/Text/CommonMark.pm
@@ -0,0 +1,28 @@
+package Text::CommonMark;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::CommonMark - Short intro
+
+=head1 SYNOPSIS
+
+  use Text::CommonMark;
+
+=head1 DESCRIPTION
+
+Text::CommonMark is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,9 @@
+use strict;
+use Test::More;
+use Text::CommonMark;
+
+# replace with the actual test
+ok 1;
+
+done_testing;
+
